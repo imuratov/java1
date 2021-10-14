70236a98a500ccfd27c86b115dcde87355a5e735
imuratov <imuratov167@gmail.com>
2021-10-14 18:13:38 +0400

    пуш 4й задачи, 1

---
+++ project1/src/com/max/operat/operat.java
@@ -0,0 +1,14 @@
+package com.max.operat;
+
+public class operat {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 2;
+        int z = 3;
+        int i = ((x + y + z)/3);
+        double p = Math.floor(i/2);
+        if (p > 3)
+            System.out.println("Программа выполнена корректно");
+
+    }
+}
