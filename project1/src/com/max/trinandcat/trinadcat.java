cb46498eed66f8a839db978ae2e6f9ec8c761780
imuratov <imuratov167@gmail.com>
2021-12-23 13:03:26 +0400

    Задача 13

---
+++ project1/src/com/max/trinandcat/trinadcat.java
@@ -0,0 +1,20 @@
+package com.max.trinandcat;
+import java.util.Scanner;
+
+public class trinadcat {
+    public static void main(String[] args) {
+        int count = 0;
+        Scanner scan = new Scanner(System.in);
+        String str = scan.nextLine();
+        String[] array = str.split("\\s+");
+
+        for (int i = 0; i < array.length; i++) {
+            boolean Alphabet = array[i].matches("^[a-zA-Z]+$");
+            if (Alphabet) {
+                count = count + 1;
+                System.out.println(array[i]);
+            }
+            System.out.println("Кол-во слов: " + count);
+        }
+    }
+}
