9f62cf3eca4b76682981bea816940613404cf0fd
imuratov <imuratov167@gmail.com>
2022-01-02 18:45:27 +0400

    Задача 8(исправлено)

--- project1/src/com/max/cikloid/cikloid.java
+++ project1/src/com/max/cikloid/cikloid.java
@@ -8,7 +8,7 @@ public class cikloid {
         int n = sc.nextInt();
         int sum = 0;
         for (int i=1; i<= n; i++){
-            if(i % 2 == 1)
+            if(!(i % 2 == 0))
             sum = sum + i;
         }
         System.out.println (sum);
