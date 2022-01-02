6fe4c277d711cc6e55d4ab84dd86d3481d325505
imuratov <imuratov167@gmail.com>
2022-01-02 21:43:47 +0400

    Итоговая 3(исправлено)

--- project1/src/com/max/stroki/stroki.java
+++ project1/src/com/max/stroki/stroki.java
@@ -1,25 +1,58 @@
 package com.max.stroki;
-
 import java.util.Scanner;
 
+
 public class stroki {
-    public static void main(String[] args) {
+    public static void main(String args [ ])
+    {
+        System.out.print("Количество строк: ");
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите количество строк: ");
-        Integer n = new Integer( scanner.next());
-        int max = 0;
-        String maxStr = "";
-        for (int i = 1; i <= n; i = i+ 1){
-            System.out.print("Строка " + i + ": ");
-            String j = scanner.next();
-            System.out.println(j);
-            System.out.println("");
-            if (max < j.length()){
-                max = j.length();
-                maxStr = j;
+        int a = scanner.nextInt();
+        String [] strArray = new String[a];
+        int [] intArray = new int[a];
+        scanner.nextLine();
+        for (int i = 0;i < strArray.length;i++){
+            System.out.print("Строка "+(i + 1)+" : ");
+            strArray[i] = scanner.nextLine();
+        }
+        System.out.print("Ответ: ");
+        int counter = 0;
+        int counter1 = 0;
+        for (int i = 0;i < strArray.length;i++) {
+            counter = 0;
+            char[] chArray = strArray[i].toCharArray();
+            for (int j = 0;j < chArray.length;j++){
+                counter1 = 0;
+                for (int k = 0;k<chArray.length;k++){
+                    if (chArray[j] == chArray[k]){
+                        counter1 += 1;
+                    }if (counter1 > 1){
+                        counter += 1;
+                    }
+                    intArray[i] = counter;
+                }
             }
 
         }
-        System.out.println("Ответ: " + maxStr );
+        int min = intArray[0];
+        for (int i = 0;i < intArray.length;i++){
+            if (intArray[i] < min){
+                min = intArray[i];
+            }
+        }
+        int counter2 = 0;
+        for(int i = 0;i < intArray.length;i++){
+            if(intArray[i] == min){
+                counter2 += 1;
+            }
+        }
+        for (int i = 0;i < intArray.length;i++){
+            if (intArray[i] == min){
+                System.out.print(strArray[i]);
+                if (counter2 > 1){
+                    break;
+                }
+            }
+        }
     }
 }
